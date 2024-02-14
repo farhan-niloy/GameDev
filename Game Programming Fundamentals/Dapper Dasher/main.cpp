31df91e3de130fb1fdec2feb1bb8014ab597ea47
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-02-14 15:27:38 +0600

    update

--- Game Programming Fundamentals/Dapper Dasher/main.cpp
+++ Game Programming Fundamentals/Dapper Dasher/main.cpp
@@ -9,23 +9,22 @@ struct AnimData {
 }; 
 
 bool isOnGround(AnimData data, int windowHeight) {
-  return data.pos.y >= windowHeight - data.rec.height;
+    return data.pos.y >= windowHeight - data.rec.height;
 }
 
-AnimData updateAnimData(AnimData data, float deltaTime, int maxFrame)
-{
-  // Update running Time
-  data.runningTime += deltaTime;
-  if (data.runningTime >= data.updateTime){
-    data.runningTime = 0.0;
-    // update Animation frame
-    data.rec.x = data.frame * data.rec.width;
-    data.frame++;
-    if(data.frame > maxFrame) {
-      data.frame = 0;
+AnimData updateAnimData(AnimData data, float deltaTime, int maxFrame) {
+    // Update running Time
+    data.runningTime += deltaTime;
+    if (data.runningTime >= data.updateTime){
+        data.runningTime = 0.0;
+        // update Animation frame
+        data.rec.x = data.frame * data.rec.width;
+        data.frame++;
+        if(data.frame > maxFrame) {
+            data.frame = 0;
+        }
     }
-  }
-  return data;
+    return data;
 }
 
 int main() {
@@ -74,18 +73,19 @@ int main() {
     AnimData nebulae[sizeOFNebulae]{};
 
     // Initialize Nebulae
-    for (int i = 0; i < sizeOFNebulae; ++i)
-{
-    nebulae[i].rec.x = 0.0f;
-    nebulae[i].rec.y = 0.0f;
-    nebulae[i].rec.width = static_cast<float>(nebula.width) / 8;
-    nebulae[i].rec.height = static_cast<float>(nebula.height) / 8;
-    nebulae[i].pos.y = static_cast<float>(WindowHeight) - static_cast<float>(nebula.height) / 8;
-    nebulae[i].frame = 0;
-    nebulae[i].runningTime = 0.0f;
-    nebulae[i].updateTime = 1.0f / 16.0f;
-    nebulae[i].pos.x = static_cast<float>(WindowWidth) + i * 300;
-}
+    for (int i = 0; i < sizeOFNebulae; ++i) {
+        nebulae[i].rec.x = 0.0f;
+        nebulae[i].rec.y = 0.0f;
+        nebulae[i].rec.width = static_cast<float>(nebula.width) / 8;
+        nebulae[i].rec.height = static_cast<float>(nebula.height) / 8;
+        nebulae[i].pos.y = static_cast<float>(WindowHeight) - static_cast<float>(nebula.height) / 8;
+        nebulae[i].frame = 0;
+        nebulae[i].runningTime = 0.0f;
+        nebulae[i].updateTime = 1.0f / 16.0f;
+        nebulae[i].pos.x = static_cast<float>(WindowWidth) + i * 300;
+    }
+
+    float finishLine = nebulae[sizeOFNebulae - 1].pos.x;
 
     bool isInAir = false;
 
@@ -103,38 +103,35 @@ int main() {
         mgX -= 40 * dT;
         fgX -= 80 * dT;
 
-        if(bgX <= -background.width*9) {
-          bgX = 0.0;
+        if (bgX <= -background.width * 9) {
+            bgX = 0.0;
         }
 
-         if(mgX <= -background.width*9) {
-          bgX = 0.0;
+        if (mgX <= -midground.width * 9) {
+            mgX = 0.0;
         }
 
-          if(fgX <= -background.width*9) {
-          bgX = 0.0;
+        if (fgX <= -foreground.width * 9) {
+            fgX = 0.0;
         }
-
-
 
         // Draw the background
         Vector2 big1Pos{bgX, 0.0f};
         DrawTextureEx(background, big1Pos, 0.0f, 9.0f, WHITE);
-        Vector2 big2Pos{bgX + background.width*9, 0.0};
+        Vector2 big2Pos{bgX + background.width * 9, 0.0};
         DrawTextureEx(background, big2Pos, 0.0, 9.0f, WHITE);
         
         // Draw the midground
         Vector2 mg1Pos{mgX, 0.0f};
         DrawTextureEx(midground, mg1Pos, 0.0f, 9.0f, WHITE);
-        Vector2 mg2Pos{mgX + midground.width*9, 0.0f};
+        Vector2 mg2Pos{mgX + midground.width * 9, 0.0f};
         DrawTextureEx(midground, mg2Pos, 0.0f, 9.0f, WHITE);
 
         // Draw the foreground
         Vector2 fg1Pos{fgX, 0.0f};
         DrawTextureEx(foreground, fg1Pos, 0.0f,  9.0f, WHITE);
-        Vector2 fg2Pos{fgX + midground.width*9, 0.0f};
+        Vector2 fg2Pos{fgX + foreground.width * 9, 0.0f};
         DrawTextureEx(foreground, fg2Pos, 0.0f, 9.0f, WHITE);
-
 
         // Ground Check
         if (isOnGround(scarfyData, WindowHeight)) {
@@ -154,33 +151,54 @@ int main() {
 
         // Update Nebula Positions
         for(int i = 0; i < sizeOFNebulae; i++) {
-          nebulae[i].pos.x += nebVel * dT;
+            nebulae[i].pos.x += nebVel * dT;
         }
+
+        // Update finishLine
+        finishLine += nebVel * dT;
 
         // Update Scarfy Position
         scarfyData.pos.y += velocity * dT;
 
         // Update Scarfy Animation Frame
-        if (!isInAir) 
-        {
-          scarfyData = updateAnimData(scarfyData, dT, 5);
+        if (!isInAir) {
+            scarfyData = updateAnimData(scarfyData, dT, 5);
         }
 
-        // Update Nebulae Animation Frames
-        for (int i = 0; i < sizeOFNebulae; ++i)
-        {
-          nebulae[i] = updateAnimData(nebulae[i], dT, 7);
+        // Check collision 
+        bool collision = false;
+        for (AnimData nebula : nebulae) {
+            float pad = 20;
+            Rectangle nebRec {
+                nebula.pos.x + pad,
+                nebula.pos.y + pad, 
+                nebula.rec.width - 2 * pad,
+                nebula.rec.height - 2 * pad
+            };
+            Rectangle scarfyRec {
+                scarfyData.pos.x,
+                scarfyData.pos.y,
+                scarfyData.rec.width,
+                scarfyData.rec.height
+            };
+
+            if (CheckCollisionRecs(nebRec, scarfyRec)) {
+                collision = true;
+                break; // Exit loop once collision is detected
+            }
         }
 
-        // Draw Nebulae
-        for (int i = 0; i < sizeOFNebulae; ++i) {
-            DrawTextureRec(nebula, nebulae[i].rec, nebulae[i].pos, ORANGE);
+        // Draw Nebulae and Scarfy if no collision
+        if (!collision) {
+            // Draw Nebulae
+            for (int i = 0; i < sizeOFNebulae; ++i) {
+                DrawTextureRec(nebula, nebulae[i].rec, nebulae[i].pos, PURPLE);
+            }
+
+            // Draw Scarfy
+            DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
         }
 
-        // Draw Scarfy
-        DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
-
-        // Stop Drawing
         EndDrawing();
     }
 
@@ -193,4 +211,5 @@ int main() {
     CloseWindow();
 
     return 0;
-}
+} 
+
